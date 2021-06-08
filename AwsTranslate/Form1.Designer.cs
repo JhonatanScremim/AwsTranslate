72f5a490ef7669ada2b0b4ef7eb8024fe75ffd68
Jhonatan Scremim <jhonatanscremim@gmail.com>
2021-06-08 20:57:10 -0300

    feat(initial-translate): inicia função do Translate

--- AwsTranslate/Form1.Designer.cs
+++ AwsTranslate/Form1.Designer.cs
@@ -29,13 +29,56 @@ namespace AwsTranslate
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.richTextBoxInput = new System.Windows.Forms.RichTextBox();
+            this.richTextBoxOutput = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // richTextBoxInput
+            // 
+            this.richTextBoxInput.Location = new System.Drawing.Point(70, 110);
+            this.richTextBoxInput.Name = "richTextBoxInput";
+            this.richTextBoxInput.Size = new System.Drawing.Size(239, 274);
+            this.richTextBoxInput.TabIndex = 0;
+            this.richTextBoxInput.Text = "";
+            // 
+            // richTextBoxOutput
+            // 
+            this.richTextBoxOutput.Location = new System.Drawing.Point(450, 110);
+            this.richTextBoxOutput.Name = "richTextBoxOutput";
+            this.richTextBoxOutput.Size = new System.Drawing.Size(241, 274);
+            this.richTextBoxOutput.TabIndex = 1;
+            this.richTextBoxOutput.Text = "";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(383, 54);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(94, 29);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.richTextBoxOutput);
+            this.Controls.Add(this.richTextBoxInput);
+            this.Name = "Form1";
+            this.Text = "FormMain";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBoxInput;
+        private System.Windows.Forms.RichTextBox richTextBoxOutput;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- AwsTranslate/Form1.cs
+++ AwsTranslate/Form1.cs
@@ -1,4 +1,10 @@
-﻿using System;
+﻿using Amazon;
+using Amazon.Runtime;
+using Amazon.Runtime.CredentialManagement;
+using Amazon.S3;
+using Amazon.Translate;
+using Amazon.Translate.Model;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -17,5 +23,50 @@ namespace AwsTranslate
             InitializeComponent();
         }
 
+        string credentialName = "AWS Educate";
+        AWSCredentials credentials;
+        private static readonly RegionEndpoint region = RegionEndpoint.USEast1;
+
+        AmazonS3Client client;
+
+        private readonly string sourceLang = "pt-br";
+        private readonly string targetLang = "en-us";
+
+
+
+        private void GetCredentials()
+        {
+            var chain = new CredentialProfileStoreChain();
+            if (!chain.TryGetAWSCredentials(credentialName, out credentials))
+            {
+                MessageBox.Show("Erro ao pegar credencial");
+            }
+        }
+
+        private void S3Client()
+        {
+            GetCredentials();
+            client = new AmazonS3Client(credentials, region);
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            GetCredentials();
+
+            using (var clientTranslate = new AmazonTranslateClient(credentials))
+            {
+                var request = new TranslateTextRequest()
+                {
+                    SourceLanguageCode = sourceLang,
+                    TargetLanguageCode = targetLang,
+                    Text = "Mesa cadeira livro campo tenis"
+                };
+
+
+                var response = await clientTranslate.TranslateTextAsync(request);
+            }
+
+
+        }
     }
 }
